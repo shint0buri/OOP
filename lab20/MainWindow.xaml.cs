b4aa88f4b6c9c7ad56da3bfa5e600128f0795d9f
shint0buri <124457998+shint0buri@users.noreply.github.com>
2025-12-30 19:17:13 +0300

    Add files via upload

---
+++ lab20/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace lab20
+{
+    
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _dynamicText = "Динамическая привязка работает!";
+        public string DynamicText
+        {
+            get => _dynamicText;
+            set
+            {
+                _dynamicText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            DataContext = this;
+        }
+
+        private void BtnDynamic_Click(object sender, RoutedEventArgs e)
+        {
+          
+            Binding binding = new Binding("DynamicText")
+            {
+                Mode = BindingMode.OneWay,
+                Source = this
+            };
+            lblDynamic.SetBinding(TextBlock.TextProperty, binding);
+        }
+
+        private void BtnSetFont_Click(object sender, RoutedEventArgs e)
+        {
+         
+            cmbFonts.SelectedItem = new ComboBoxItem { Content = "Courier New" };
+        }
+    }
+}
